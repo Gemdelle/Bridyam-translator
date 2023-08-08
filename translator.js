c7fa298ea63ec5c00f8cbda22816d75f4728b085
Gem <milu200911@hotmail.com>
2023-08-08 11:37:36 -0300

    rule 01.

--- translator.js
+++ translator.js
@@ -1,6 +1,38 @@
 // RULES
 
 // 01. 
+/* 01. Identificar las vocales e intercambiarlas
+Za pa ti lla → a a i a → a i a a
+Si solo se tienen 3 vocales y la 1era y la 3era son iguales, se intercambian con la intermedia
+A ni mal → a i a → i a i  */
+
+let word = 'amor';
+word = word.slice('');
+console.log(word);()
+
+const vocals = ['a','e','i','o','u'];
+
+// Eliminar la última letra si es una vocal
+if(vocals.includes(word[word.length-1])){ 
+  word.pop()            
+} 
+
+// Agregar la última consonante después de la primera vocal
+let last_consonant = word[word.length-1]
+console.log(last_consonant)
+  // hay que buscar la primera vocal y agregar esa letra después
+for(i = 0;i < word.length-1;i++) {
+  if
+}
+
+
+// Intercambiar las vocales entre sí
+
+// Después vamos a agregar algunas vocales al final pero depende de cómo termine la palabra
+
+console.log(word)
+
+
 
 // 02.
 
